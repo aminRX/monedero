38b42aaf5ee350ca553ffdc8d118213a88779170
Amin Ogarrio <amin.ogarrio@gmail.com>
2017-01-17 11:19:53 -0700

    Created roles model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170117173801) do
+ActiveRecord::Schema.define(version: 20170117181616) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "roles", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_roles_on_name", unique: true, using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "userid"
@@ -18,8 +28,8 @@ ActiveRecord::Schema.define(version: 20170117173801) do
     t.string   "auth_token"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.index ["auth_token"], name: "index_users_on_auth_token", unique: true
-    t.index ["userid"], name: "index_users_on_userid", unique: true
+    t.index ["auth_token"], name: "index_users_on_auth_token", unique: true, using: :btree
+    t.index ["userid"], name: "index_users_on_userid", unique: true, using: :btree
   end
 
 end
