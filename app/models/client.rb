5c2a359cc8e0dea62780605ae506e6a89f80621f
Amin Ogarrio <amin.ogarrio@gmail.com>
2017-01-23 22:41:50 -0700

    Nested client for client profile.

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,5 @@
 class Client < ApplicationRecord
   belongs_to :user
   has_one :client_profile
+  accepts_nested_attributes_for :client_profile
 end
--- app/models/client_profile.rb
+++ app/models/client_profile.rb
@@ -1,3 +1,3 @@
 class ClientProfile < ApplicationRecord
-  belongs_to :clients
+  belongs_to :clients, optional: true
 end
