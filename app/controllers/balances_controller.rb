5bb6047c18545ea1134c3e43d4a2bce63dd82efd
Amin Ogarrio <amin.ogarrio@gmail.com>
2017-01-26 22:41:51 -0700

    Added create a balance.

--- app/controllers/balances_controller.rb
+++ app/controllers/balances_controller.rb
@@ -1,11 +1,25 @@
 class BalancesController < ApplicationController
   def new
-    user = current_user
-    if user
-
-    end
   end
 
   def create
+    user = current_user
+    if user
+      client = user.clients.find_by_id params[:client_id]
+      balances = client.balances.new balance_params
+      if balances.save
+        flash[:points]
+        redirect_to client_path(params[:client_id])
+      else
+        flash[:invalid]
+        redirect_to client_path(params[:client_id])
+      end
+    end
+  end
+
+  private
+
+  def balance_params
+    params.require(:balance).permit(:point)
   end
 end
