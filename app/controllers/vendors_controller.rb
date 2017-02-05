f9e7f3eab3bc6fe36701f28454033789c4017bf6
Amin Ogarrio <amin.ogarrio@gmail.com>
2017-02-05 03:06:50 -0700

    Create vendors.

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -1,8 +1,19 @@
 class VendorsController < ApplicationController
+  before_action :login
   def new
+    user = current_user
+    @vendor = user.vendors.new
   end
 
   def create
+    @vendor = current_user.vendors.new vendor_params
+    if @vendor.save
+      flash[:vendor_created] = "Usuario #{@vendor[:name]} creado."
+      redirect_to vendors_path
+    else
+      flash[:vendor_error] = "Error al crear el usuario."
+      redirect_to new_vendor_path
+    end
   end
 
   def index
@@ -15,5 +26,11 @@ class VendorsController < ApplicationController
   end
 
   def delete
+  end
+
+  private
+
+  def vendor_params
+    params.require(:vendor).permit(:name)
   end
 end
