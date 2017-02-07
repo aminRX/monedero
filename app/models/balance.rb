aad5ad66e0af1f807b27901918423d1f7431e7d6
Amin Ogarrio <amin.ogarrio@gmail.com>
2017-02-06 21:25:36 -0700

    Momentary optional vendor belongs_to.

--- app/models/balance.rb
+++ app/models/balance.rb
@@ -1,6 +1,6 @@
 class Balance < ApplicationRecord
   belongs_to :client
-  belongs_to :vendor
+  belongs_to :vendor, optional: true
 
   def self.sufficient_balance?(points)
     points <= self.sum(:point) ? true : false
