dd41798e3d6b7f37af88c6778d61fb48424e2221
OmarRx <omarrx14@gmail.com>
2017-02-11 20:20:30 -0700

    Added email to cliente profile.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170211074213) do
+ActiveRecord::Schema.define(version: 20170212025716) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
