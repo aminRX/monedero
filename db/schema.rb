d66f96e5dab27458bf0ef19cd27adc2d5cf24d8d
OmarRx <omarrx14@gmail.com>
2017-02-13 14:16:31 -0700

    Added final design to balances.

--- db/schema.rb
+++ db/schema.rb
@@ -88,6 +88,7 @@ ActiveRecord::Schema.define(version: 20170213071714) do
     t.string   "auth_token"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "roles_id"
     t.integer  "role_id"
     t.index ["auth_token"], name: "index_users_on_auth_token", unique: true, using: :btree
     t.index ["role_id"], name: "index_users_on_role_id", using: :btree
