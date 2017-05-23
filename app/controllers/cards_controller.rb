d7455463dc08e90b900f31d8c3e1a8018773f3a4
Amin Ogarrio <amin.ogarrio@gmail.com>
2017-05-22 17:58:00 -0700

    Spanglish

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -29,7 +29,7 @@ class CardsController < ApplicationController
 
     respond_to do |format|
       if @card.save
-        format.html { redirect_to @card, notice: 'Card was successfully created.' }
+        format.html { redirect_to @card, notice: 'Tarjeta fue creada exitosamente.' }
         format.json { render :show, status: :created, location: @card }
       else
         format.html { render :new }
@@ -43,7 +43,7 @@ class CardsController < ApplicationController
   def update
     respond_to do |format|
       if @card.update(card_params)
-        format.html { redirect_to @card, notice: 'Card was successfully updated.' }
+        format.html { redirect_to @card, notice: 'Tarjeta fue modificada.' }
         format.json { render :show, status: :ok, location: @card }
       else
         format.html { render :edit }
@@ -57,7 +57,7 @@ class CardsController < ApplicationController
   def destroy
     @card.destroy
     respond_to do |format|
-      format.html { redirect_to cards_url, notice: 'Card was successfully destroyed.' }
+      format.html { redirect_to cards_url, notice: 'Tarjeta fue destruida.' }
       format.json { head :no_content }
     end
   end
